9de3bd208b01914b905eb9a365dd7526411272cc
Nicky Liu <nickyliu91@gmail.com>
2019-07-29 03:58:41 -0400

    all menu items added

--- src/component/Home.js
+++ src/component/Home.js
@@ -41,14 +41,12 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <div>
-        <div id="home">
-          <img className="restaurant" src={`images/restaurant` + `${this.state.imageNumber}` + `.jpg`} onClick={this.clickChangePicture}/>
-          <div>
-            <p>We’ll provide our customers with great food and hospitality. They will leave happy, satisfied, and eager to pay us another visit.</p>
-            <p>We are great people delivering quality food and service</p>
-            <p>We will provide amazing food and service, without the usual prices attached to amazing food and service.</p>
-          </div>
+      <div id="home">
+        <img className="restaurant" src={`images/restaurant` + `${this.state.imageNumber}` + `.jpg`} onClick={this.clickChangePicture}/>
+        <div>
+          <p>We’ll provide our customers with great food and hospitality. They will leave happy, satisfied, and eager to pay us another visit.</p>
+          <p>We are great people delivering quality food and service</p>
+          <p>We will provide amazing food and service, without the usual prices attached to amazing food and service.</p>
         </div>
       </div>
     )
